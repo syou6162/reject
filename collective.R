6348937869123746df1c2bad46079ff75b378402
Yasuhisa Yoshida <syou6162@gmail.com>
2008-08-17 08:24:42 +0900

    単語の登場回数とblogのmatrixを返す関数を作成。

--- collective.R
+++ collective.R
@@ -16,5 +16,37 @@ blog = Blog.new
 blog.write_blogs_from_opml(blog.get_blogs_from_opml)
 ",sep="")
   system(paste("echo '",ruby,"'"," | /opt/local/bin/ruby ",sep=""))
-  return(read.csv("tmp.csv",header=FALSE,sep="\t"))
+  d <- read.csv("tmp.csv",header=FALSE,sep="\t",stringsAsFactors=FALSE)
+  names(d) <- c("title","url")
+  return(d)
+}
+
+
+#blogとキーワードの行列を生成する(本当はデータフレームになっている)
+get_data_frame_of_words_from_urls <- function(urls){
+  #それぞれのフィードに入っているキーワードの個数のベクトルが入っているリスト
+  make_lists_of_words_from_urls <- function(urls){
+    a <- list()
+    for(url in urls){
+      cat(url,fill=T)
+      word <- table(extract_meisi_from_blog(url))
+      b <- list(word[order(word,decreasing=TRUE)])
+      a <- append(a,b)  
+    }
+    return(a)
+  }
+  a <- make_lists_of_words_from_urls(urls$url)
+  #Iは因子化を防ぐための操作
+  d <- data.frame(words=I(unique(unlist(mapply(names,a)))))
+  for(i in seq(length(a))){
+    d[,i+1] <- as.numeric(mapply(function(x){ifelse(!is.na(a[[i]][x]),a[[i]][x],0)},d$words))
+  }
+  #keywordをrow.namesにして、データフレームから出しておく
+  row.names(d) <- d$words
+  d$words <- NULL
+  #colのnameのほうはblogのurlにしておく
+  names(d) <- urls$title
+  #単語の登場回数が0なものを列から取り除く
+  d <- d[,as.vector(apply(d,2,function(x){!all(x == 0)}))]
+  return(d)
 }
--- main.R
+++ main.R
@@ -9,33 +9,20 @@ head(extract_meisi_from_blog("http://d.hatena.ne.jp/syou6162/rss"))
 
 tail(get_blogs_from_opml())$V2
 
-#それぞれのフィードに入っているキーワードの個数のベクトルが入っているリスト
-a <- list()
 
-for(url in as.character(get_blogs_from_opml()$V2)[1:20]){
-  cat(url,fill=T)
-  word <- table(extract_meisi_from_blog(url))
-  b <- list(word[order(word,decreasing=TRUE)])
-  a <- append(a,b)  
-}
 
-#キーワードが全部入っているデータフレームを生成する
-hoge <- data.frame(words=I(unique(unlist(mapply(names,a)))))
+aaa <- get_blogs_from_opml()
 
-#blogとキーワードの行列を生成する(本当はデータフレームになっている)
-for(i in seq(length(a))){
-  hoge[,i+1] <- as.numeric(mapply(function(x){ifelse(!is.na(a[[i]][x]),a[[i]][x],0)},hoge$words))
-}
+str(head(aaa))
+str(get_data_frame_of_words_from_urls(get_blogs_from_opml()[1:5,]))
 
-head(hoge,n=30)
+
+
+
+
+head(hoge,n=3)
 summary(hoge)
 
-#keywordをrow.namesにして、データフレームから出しておく
-row.names(hoge) <- hoge$words
-hoge$words <- NULL
-
-#colのnameのほうはblogのurlにしておく
-names(hoge) <- as.character(get_blogs_from_opml()$V2)[1:20]
 as.character(get_blogs_from_opml()$V2)[1:20]
 
 hoge$"http://www.6sese.info/wordpress/index.php/feed"
@@ -43,8 +30,6 @@ head(hoge[,1:3])
 
 summary(hoge)
 
-#単語の登場回数が0なものを列から取り除く
-hoge <- hoge[,as.vector(apply(hoge,2,function(x){!all(x == 0)}))]
 
 names(hoge)
 names(hoge) <- NULL
@@ -62,3 +47,6 @@ tail(sort(hoge[,1]))
 
 summary(hoge[,1])
 summary(hoge[,8])
+
+
+
